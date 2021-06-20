c9d5d9a15c4c7b659e60246695c555deeff2f9e2
Ali Ismail <ali.ai@live.com>
2021-06-20 14:33:20 +0300

    test graphsaint

---
+++ test/test_graph_saint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=test_graph_saint
+#SBATCH --partition=arza
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu
+#SBATCH --mem=0
+#SBATCH --time=1-00:00:00
+
+module purge
+module load python/3.8.2
+module load torch/1.7.1-py38-gcc-7.2.0-cuda-10.1-openmpi-4.0.1
+module load cuda
+
+python3 test_graph_saint.py
