54dfcafba0cc08c6ccff845aab3e4fde80ae2973
Ali Ismail <ali.ai@live.com>
2021-06-30 13:11:24 +0300

    fixed file selection

--- dataset.py
+++ dataset.py
@@ -85,7 +85,7 @@ class xBD(Dataset):
         zones = self.labels['zone'].value_counts()[self.labels['zone'].value_counts()>1].index.tolist()
         for zone in zones:
             if not (self.labels[self.labels['zone'] == zone]['class'] == 'un-classified').all() or \
-               not (self.labels[self.labels['zone'] == zone]['class'] != 'un-classified').sum() == 1:
+            not (self.labels[self.labels['zone'] == zone]['class'] != 'un-classified').sum() == 1:
                 processed_files.append(os.path.join(self.processed_dir, f'{zone}.pt'))
         return processed_files
 
