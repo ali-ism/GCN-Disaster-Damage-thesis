d47dff31b90ed986180b1c3a975961e189a88c33
Ali Ismail <ali.ai@live.com>
2021-07-06 13:34:52 +0300

    fixed parse_ordinal_outputs to return numpy array

--- metrics.py
+++ metrics.py
@@ -1,12 +1,13 @@
+import numpy as np
 import torch
 from sklearn.metrics import f1_score
 
 
-def parse_ordinal_output(out: torch.Tensor) -> torch.Tensor:
+def parse_ordinal_output(out: torch.Tensor) -> np.ndarray:
     idx = torch.where(out < 0.5, 1, 0)
     idx = torch.argmax(idx, dim=1)
     idx = torch.where(idx == 0, 4, idx)
-    return idx
+    return idx.detach().numpy()
 
 
 def xview2_f1_score(y_true: torch.Tensor, out: torch.Tensor) -> float:
@@ -18,8 +19,8 @@ def xview2_f1_score(y_true: torch.Tensor, out: torch.Tensor) -> float:
         y_true (torch.Tensor)
         out (torch.Tensor)
     """
-    y_pred = parse_ordinal_output(out).detach().numpy()
-    y_true = parse_ordinal_output(y_true).detach().numpy()
+    y_pred = parse_ordinal_output(out)
+    y_true = parse_ordinal_output(y_true)
     f1_classes = f1_score(y_true, y_pred, average=None)
     epsilon = 1e-6
     return len(f1_classes) / sum((f1+epsilon)**-1 for f1 in f1_classes)
