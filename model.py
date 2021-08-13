dbfebf8587cd5aa518b0ddd9fdd252708ac1391b
Ali Ismail <ali.ai@live.com>
2021-08-13 14:35:07 +0300

    new exp

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ class DeeperGCN(Module):
         self.dropout_rate = dropout_rate
         self.node_encoder = Linear(num_node_features, hidden_channels)
         ################################################################
-        self.edge_encoder = Linear(num_edge_features-1, hidden_channels)
+        #self.edge_encoder = Linear(num_edge_features-1, hidden_channels)
         ################################################################
         self.layers = ModuleList()
         for i in range(1, num_layers + 1):
@@ -31,13 +31,13 @@ class DeeperGCN(Module):
 
     def forward(self, x, edge_index, edge_attr):
         ##########################################
-        edge_attr = edge_attr[:,0].unsqueeze(dim=1)
+        #edge_attr = edge_attr[:,0].unsqueeze(dim=1)
         ###########################################
         x = self.node_encoder(x)
-        edge_attr = self.edge_encoder(edge_attr)
-        x = self.layers[0].conv(x, edge_index, edge_attr)
+        #edge_attr = self.edge_encoder(edge_attr)
+        x = self.layers[0].conv(x, edge_index)#, edge_attr)
         for layer in self.layers[1:]:
-            x = layer(x, edge_index, edge_attr)
+            x = layer(x, edge_index)#, edge_attr)
         x = self.layers[0].act(self.layers[0].norm(x))
         x = F.dropout(x, p=self.dropout_rate, training=self.training)
         x = self.lin(x)
