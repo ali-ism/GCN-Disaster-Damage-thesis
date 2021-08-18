ef2ea417b29faf3ef046b7d677c55372cd455fea
Ali Ismail <ali.ai@live.com>
2021-08-18 11:17:54 +0300

    switched to resnet34

--- README.md
+++
@@ -1,8 +0,0 @@
-# Experiments
-
-| Train                                                                          | Test                  |
-|--------------------------------------------------------------------------------|-----------------------|
-| Socal fire + Portugal wildfire                                                 | Socal fire            |
-| Sunda tsunami                                                                  | Socal fire            |
-| Sunda tsunami + Tucaloosa tornado + Puna volcano                               | Socal fire            |
-| Sunda tsunami + Tucaloosa tornado + Puna volcano + 10% Socal Fire              | 90% Socal fire        |
--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 from torch.nn import Module, ModuleList, Sequential, Linear, LayerNorm, ReLU
-from torchvision.models import resnet50
+from torchvision.models import resnet34
 from torch_geometric.nn import GENConv, DeepGCNLayer, SplineConv, GCNConv, BatchNorm
 
 
@@ -123,29 +123,29 @@ class DeeperGCN(Module):
         return F.log_softmax(x, dim=1)
 
 
-class SiameseResnetEncoder(Module):
+class SiameseEncoder(Module):
     def __init__(self):
         super().__init__()
-        self.resnet = resnet50(pretrained=True, progress=False)
-        self.resnet = Sequential(*(list(self.resnet.children())[:-1]))
+        self.model = resnet34(pretrained=True, progress=False)
+        self.model = Sequential(*(list(self.model.children())[:-1]))
     
     def forward(self, x: torch.Tensor):
         x = x.reshape((-1, 6, 128, 128))
         x1 = x[:,:3,:,:]
         x2 = x[:,3:,:,:]
-        x1 = self.resnet(x1)
-        x2 = self.resnet(x2)
+        x1 = self.model(x1)
+        x2 = self.model(x2)
         x = torch.add(input=x1, other=x2, alpha=-1)
         return x.flatten(start_dim=1)
 
-class SiameseResnetClf(Module):
+class SiameseClf(Module):
     def __init__(self, hidden_channels: int, num_classes: int, dropout_rate: float):
         super().__init__()
         
         self.dropout_rate = dropout_rate
 
-        self.encoder = SiameseResnetEncoder()
-        self.fc1 = Linear(2048, hidden_channels)
+        self.encoder = SiameseEncoder()
+        self.fc1 = Linear(512, hidden_channels)
         self.bn1 = BatchNorm(hidden_channels)
         self.fc2 = Linear(hidden_channels, hidden_channels)
         self.bn2 = BatchNorm(hidden_channels)
@@ -176,10 +176,10 @@ class CNNGCN(Module):
 
         self.dropout_rate = dropout_rate
         self.fc_output = fc_output
-        self.node_encoder = SiameseResnetEncoder()
+        self.node_encoder = SiameseEncoder()
         self.convs = ModuleList()
         self.batch_norms = ModuleList()
-        self.convs.append(GCNConv(2048, hidden_channels))
+        self.convs.append(GCNConv(512, hidden_channels))
         self.batch_norms.append(BatchNorm(hidden_channels))
         for _ in range(num_layers - 2):
             self.convs.append(GCNConv(hidden_channels, hidden_channels))
