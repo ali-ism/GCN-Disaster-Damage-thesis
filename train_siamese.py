bc007297a6bc7abd63bb79b39bce3319371c8b78
Ali Ismail <ali.ai@live.com>
2021-09-24 13:06:55 +0300

    fixed make_plot call

--- train_siamese.py
+++ train_siamese.py
@@ -30,7 +30,7 @@ train_disasters = settings_dict['data']['train_disasters']
 train_paths = settings_dict['data']['train_paths']
 merge_classes = settings_dict['data']['merge_classes']
 n_epochs = settings_dict['epochs']
-starting_epoch = 6 #settings_dict['starting_epoch']
+starting_epoch = 9 #settings_dict['starting_epoch']
 
 
 to_tensor = ToTensor()
@@ -148,7 +148,7 @@ def save_results(hold: bool=False) -> None:
     make_plot(train_acc, test_acc, 'accuracy', name)
     make_plot(train_f1_macro, test_f1_macro, 'macro_f1', name)
     make_plot(train_f1_weighted, test_f1_weighted, 'weighted_f1', name)
-    make_plot(train_auc, test_auc, 'auc')
+    make_plot(train_auc, test_auc, 'auc', name)
     np.save('results/'+name+'_loss_train.npy', train_loss)
     np.save('results/'+name+'_loss_test.npy', test_loss)
     np.save('results/'+name+'_acc_train.npy', train_acc)
